e856d6e9b18e7c0ee7440f37f824fe3d6173c0bc
alvezgr <alvezpope@gmail.com>
2019-08-10 19:12:02 -0300

    main function was created

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import (
+    Flask,
+    request,
+    make_response,
+    redirect,
+    render_template
+    )
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    print(f'ip es:{user_ip}')
+    return render_template('hello.html', user_ip=user_ip)
