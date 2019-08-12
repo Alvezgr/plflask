b0f19bf62fc0005d8c32fb6c8b7a5785c00f174a
alvezgr <alvezpope@gmail.com>
2019-08-12 16:03:49 -0300

    app config was modify

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from flask import (
     request,
     make_response,
     redirect,
-    render_template
+    render_template,
+    session
     )
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 TODO = ['Get some coffe', 'wash dishes', 'buy a dog']
 
@@ -18,14 +20,14 @@ def not_found(error):
     return render_template('404.html', error=error)
 
 @app.errorhandler(500)
-def not_found(error):
+def server_error(error):
     return render_template('500.html', error=error)
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
@@ -33,7 +35,7 @@ def index():
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': TODO
