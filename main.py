9f5f6a6fd36ea69df6d115dd1978744e2e32d91e
alvezgr <alvezpope@gmail.com>
2019-08-12 16:26:48 -0300

    flask-wtf was added, login form created

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""Flask App trainign course"""
+
 from flask import (
     Flask,
     request,
@@ -7,6 +9,9 @@ from flask import (
     session
     )
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -14,6 +19,16 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 TODO = ['Get some coffe', 'wash dishes', 'buy a dog']
+
+
+class LoginForm(FlaskForm):
+    """Class for login form"""
+
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -36,8 +51,10 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos': TODO
+        'todos': TODO,
+        'login_form': login_form
         }
     return render_template('hello.html', **context)
