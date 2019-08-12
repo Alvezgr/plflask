64739c895e3d34f44e1bb47a04a34c10f5472691
alvezgr <alvezpope@gmail.com>
2019-08-12 16:33:24 -0300

    refactor code with pylint

--- main.py
+++ main.py
@@ -13,43 +13,42 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+APP = Flask(__name__)
+bootstrap = Bootstrap(APP)
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+APP.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 TODO = ['Get some coffe', 'wash dishes', 'buy a dog']
 
 
 class LoginForm(FlaskForm):
     """Class for login form"""
-
     username = StringField('User Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
-
-@app.errorhandler(404)
+@APP.errorhandler(404)
 def not_found(error):
+    """Error handler for not found"""
     return render_template('404.html', error=error)
 
-@app.errorhandler(500)
+@APP.errorhandler(500)
 def server_error(error):
+    """Error handler for internal server error"""
     return render_template('500.html', error=error)
 
-@app.route('/')
+@APP.route('/')
 def index():
+    """the index view"""
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
     session['user_ip'] = user_ip
-
     return response
 
-
-
-@app.route('/hello')
+@APP.route('/hello')
 def hello():
+    """In hello we display the ip"""
     user_ip = session.get('user_ip')
     login_form = LoginForm()
     context = {
